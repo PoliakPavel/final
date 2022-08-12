7b3b520134090b1088257c2c732a88abde44d9bc
PoliakPavel <pashkapol27@gmail.com>
2022-08-12 13:24:08 +0300

    добавил read more добавил форму связанную с моделью на длбавление новости

--- my_site/news/views.py
+++ my_site/news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
+from .forms import *
 
 
 def index(request):
@@ -29,3 +30,14 @@ def view_news(request, news_id):
         'news_item': news_item,
     }
     return render(request, 'news/view_news.html', context)
+
+
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = form.save()
+            return redirect(news)
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
