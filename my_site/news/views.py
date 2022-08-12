7cfaec1e85f2f6d7bbbe08af9699167d6397b485
PoliakPavel <pashkapol27@gmail.com>
2022-08-12 16:00:07 +0300

    Listview (опробован - работает)

--- my_site/news/views.py
+++ my_site/news/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
 from .models import *
 from .forms import *
 
 
-def index(request):
-    news = News.objects.all()
-    context = {
-        'news': news,
-        'title': 'Список новостей',
-     }
-    return render(request, 'news/index.html', context)
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/index.html'  #по дефолту news_list.html
+    context_object_name = 'news'  #по дефолту object_list
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)    #чтобы отображались только опубликованные
+
+
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/index.html'  # сделаем что шаблон одинаковай
+    context_object_name = 'news'  # по дефолту object_list
+    allow_empty = False    #чтоб на несуществующих страницах показывало 404 вместо 500 ошибки
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
+
 
 
 def get_category(request, category_id):
